6dc17cbcf0c084ec3f0ef89b143a715c569992b3
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-01-10 21:36:11 +0600

    Update and rename trap_the_king_draft.cpp to trap_the_king.cpp

--- Chess/trap_the_king.cpp
+++ Chess/trap_the_king.cpp
@@ -26,80 +26,83 @@ void printboard(const vector<vector<char>>& board){
     }
 }
 
-void trapKing(vector<vector<char>>& board, int king_row, int king_col) {
-    // Set 1: Queen
-    // for all dark green
-    if (king_row == 1 && king_col == 1) {
+void trapKing(vector<vector<char>>& board, int king_row, int king_col){
+    // Set 1: 1 Queen
+    // for all dark green squares
+    if(king_row == 0 && king_col == 0){
         board[king_row + 1][king_col + 2] = 'Q';
     }
-    else if (king_row == 1 && king_col == 8) {
+    else if(king_row == 0 && king_col == 7){
         board[king_row + 1][king_col - 2] = 'Q';
     }
-    else if (king_row == 8 && king_col == 1) {
+    else if(king_row == 7 && king_col == 0){
         board[king_row - 1][king_col + 2] = 'Q';
     }
-    else if (king_row == 8 && king_col == 8) {
+    else if(king_row == 7 && king_col == 7){
         board[king_row - 1][king_col - 2] = 'Q';
     }
 
+    // set 2: 1 Queen, 1 Rook
     // For all Orange colored Squares
-    else if (king_col == 1) {
+    else if(king_col == 0){
         board[king_row - 1][3] = 'Q';
         board[king_row + 1][3] = 'R';
     }
-    else if (king_row == 1) {
+    else if(king_row == 0){
         board[3][king_col - 1] = 'Q';
         board[3][king_col + 1] = 'R';
     }
-    else if (king_col == 8) {
+    
+    else if(king_col == 7){
         board[king_row - 1][6] = 'Q';
         board[king_row + 1][6] = 'R';
     }
-    else if (king_row == 8) {
+    else if(king_row == 7){
         board[6][king_col - 1] = 'Q';
         board[6][king_col + 1] = 'R';
     }
 
+    // Set 3: 1 Queen, 1 Rook, 1 Bishop
     // For all Sky Blue colored Squares
-    else if (king_row == 2 && king_col == 2) {
-        board[1][4] = 'Q';
-        board[3][4] = 'R';
-        board[4][3] = 'B';
+    else if(king_row == 1 && king_col == 1){
+        board[0][3] = 'Q';
+        board[2][3] = 'R';
+        board[3][2] = 'B';
     }
-    else if (king_row == 2 && king_col == 7) {
-        board[1][5] = 'Q';
-        board[3][5] = 'R';
-        board[4][6] = 'B';
+    else if(king_row == 1 && king_col == 6){
+        board[0][4] = 'Q';
+        board[2][4] = 'R';
+        board[3][5] = 'B';
     }
-    else if (king_row == 7 && king_col == 2) {
-        board[8][4] = 'Q';
-        board[6][4] = 'R';
-        board[5][3] = 'B';
+    else if(king_row == 6 && king_col == 1){
+        board[7][3] = 'Q';
+        board[5][3] = 'R';
+        board[4][2] = 'B';
     }
-    else if (king_row == 7 && king_col == 7) {
-        board[8][5] = 'Q';
-        board[6][5] = 'R';
-        board[5][6] = 'B';
+    else if(king_row == 6 && king_col == 6){
+        board[7][4] = 'Q';
+        board[5][4] = 'R';
+        board[4][5] = 'B';
     }
 
     // For all light Green colored Squares
-    else if (king_row == 2 || king_row == 7 || king_col == 2 || king_col == 7) {
-        if (king_row == 2) {
+    else if(king_row == 1 || king_row == 6 || king_col == 1 || king_col == 6){
+        if(king_row == 1){
             board[king_row - 1][king_col + 2] = 'Q';
             board[king_row + 2][king_col - 1] = 'R';
             board[king_row + 2][king_col] = 'B';
         }
-        else if (king_row == 7) {
+        else if(king_row == 6){
             board[king_row + 1][king_col + 2] = 'Q';
             board[king_row - 2][king_col - 1] = 'R';
             board[king_row - 2][king_col] = 'B';
         }
-        else if (king_col == 2) {
+        else if(king_col == 1){
             board[king_row - 1][king_col + 2] = 'Q';
             board[king_row + 1][king_col + 2] = 'R';
             board[king_row + 2][king_col + 1] = 'B';
         }
-        else {
+        else{
             board[king_row - 1][king_col - 2] = 'Q';
             board[king_row + 1][king_col - 2] = 'R';
             board[king_row + 2][king_col - 1] = 'B';
@@ -113,11 +116,6 @@ void trapKing(vector<vector<char>>& board, int king_row, int king_col) {
         board[king_row + 2][king_col + 1] = 'B';
     }
 }
-void solution(){
-    
-
-    
-}
  
 signed main(){
     
@@ -126,11 +124,13 @@ signed main(){
 
     int king_row,king_col;
     cin>>king_row>>king_col;
+
     king_row--;
     king_col--;
 
     vector<vector<char>>board(board_size, vector<char>(board_size, '*'));
     board[king_row][king_col] = 'K';
+
     // To print the board
     // for(int i=0; i<board_size; i++){
     // // for(int i=board_size-1; i>=0; i--){
@@ -139,13 +139,12 @@ signed main(){
     //     }
     //     cout<<nl;
     // }
-    // To print the board
-    //printboard(board);
 
-    //trapKing();
+    trapKing(board, king_row, king_col);
+
+    printboard(board);
+
     ISO();
- 
-    solution();
- 
+
     return 0;
 }
