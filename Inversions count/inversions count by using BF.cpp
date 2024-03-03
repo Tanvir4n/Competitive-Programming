09a5f6ff4b29be0de03a06dec3fede4a255a2db2
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-03-03 20:38:19 +0600

    Update inversions count by using BF.cpp

--- Inversions count/inversions count by using BF.cpp
+++ Inversions count/inversions count by using BF.cpp
@@ -1,7 +1,7 @@
 int inv=0;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
+for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
                 if(v[i] > v[j]){
                     inv++;
-                }
-            }
+        }
+}
