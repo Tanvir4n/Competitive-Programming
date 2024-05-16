e8303a94c0f623727eb6d26500a8d192d18f1a33
Md. Tanvir Hasan <114309937+Tanvir4n@users.noreply.github.com>
2024-05-17 01:10:34 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define ll long long
+#define ull unsigned long long
+#define ui unsigned int
+#define pi acos(-1)
+#define pb push_back
+#define endl "\n"
+#define nl "\n"
+
+void FastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solution(){
+    
+    
+}
+ 
+signed main(){
+ 
+    FastIO();
+    solution();
+ 	
+
+
+    return 0;
+}
